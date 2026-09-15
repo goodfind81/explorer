206dd19c2eff48db3b770e0ef6103d29d3ce08d0
goodfind81 <goodfind81@gmail.com>
2026-09-14 17:21:15 -0700

    updated for more chapter

--- data/science/subject.js
+++ data/science/subject.js
@@ -1,8 +1,5 @@
 /* ==========================================================
    Science subject registry.
-   This file is the ONLY place that knows which chapters
-   belong to Science. Adding a new chapter = import it here
-   and add it to the `chapters` array.
    ========================================================== */
 
 import { chapter as ch1 } from "./chapter-01-scientific-method.js";
@@ -11,5 +8,7 @@ import { chapter as ch3 } from "./chapter-03-inferences.js";
 import { chapter as ch4 } from "./chapter-04-qual-quant.js";
 import { chapter as ch5 } from "./chapter-05-measurement.js";
 import { chapter as ch6 } from "./chapter-06-variables.js";
+import { chapter as ch7 } from "./chapter-07-data-and-graphs.js";
+import { chapter as ch8 } from "./chapter-08-bouncy-ball-lab.js";
 
-export const chapters = [ch1, ch2, ch3, ch4, ch5, ch6];
+export const chapters = [ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8];
