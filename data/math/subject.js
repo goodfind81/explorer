3fc60e6c445805f2c53554f137f4a8d3f5e200be
goodfind81 <goodfind81@gmail.com>
2026-09-15 18:49:51 -0700

    fix

--- data/math/subject.js
+++ data/math/subject.js
@@ -1,8 +1,5 @@
 /* ==========================================================
    Math subject registry.
-   This file is the ONLY place that knows which chapters
-   belong to Math. Adding a new chapter = import it here
-   and add it to the `chapters` array.
    ========================================================== */
 
 import { chapter as ch1 } from "./chapter-01-patterns-exponents.js";
@@ -12,5 +9,6 @@ import { chapter as ch4 } from "./chapter-04-decimal-place-value.js";
 import { chapter as ch5 } from "./chapter-05-compare-decimals.js";
 import { chapter as ch6 } from "./chapter-06-round-decimals.js";
 import { chapter as ch7 } from "./chapter-07-topic-1-review.js";
+import { chapter as ch8 } from "./chapter-08-multiply-powers-of-10.js";
 
-export const chapters = [ch1, ch2, ch3, ch4, ch5, ch6, ch7];
+export const chapters = [ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8];
